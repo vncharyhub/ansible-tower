e42fcf9658cdf66ce4b7ad98217938373229168e
ananya <ananya@gmail.com>
2025-08-04 14:31:15 +0530

    updating app

--- roles/python_webapp/files/app.py
+++ roles/python_webapp/files/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello from Flask via Nginx + Gunicorn + Ansible! New"
+    return "🚀 Hello from Flask via Nginx + Gunicorn + Ansible! New Version V1"
 
 if __name__ == "__main__":
     app.run()
