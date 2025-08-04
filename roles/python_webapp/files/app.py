46766430288734fa4c06c3359be757fad372364d
ananya <ananya@gmail.com>
2025-08-04 17:57:55 +0530

    animations added to app

--- roles/python_webapp/files/app.py
+++ roles/python_webapp/files/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "🚀 Hello DevOps Engineer, learning Ansible Tower AWX UI automation done SUCCESS"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
