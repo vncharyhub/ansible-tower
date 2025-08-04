61a8f30be648e3ecfad68f4ae6934032bb484709
ananya <ananya@gmail.com>
2025-08-04 18:06:35 +0530

    animations added to app

--- roles/python_webapp/files/app.py
+++ roles/python_webapp/files/app.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template
-import os
-
-# Set the base directory of the script
-base_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Point Flask to the templates folder explicitly
-app = Flask(__name__, template_folder=os.path.join(base_dir, 'templates'))
+from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "🚀 Hello DevOps Engineer, learning Ansible Tower AWX UI automation SUCCESSFULLY COMPLETED"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
